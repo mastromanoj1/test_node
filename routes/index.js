87d1027a3e558dfc30eb0f579cb57946d4710914
mastromanoj1 <mastromanoj1@gmail.com>
2023-02-27 15:58:30 +0530

    console

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,9 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send('Welcome pageee')
+  const i = 0;
+  console.log(i , ":num");
+  i++;
   // res.render('index', { title: 'Express' });
 });
 
